bcbec2cb8bdb23fccbd8335d828a1b1973e3d007
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2023-06-16 22:18:17 +0530

    home page day3

--- Frontend/scripts/index.js
+++ Frontend/scripts/index.js
@@ -1,16 +1,70 @@
 
+// localStorage.setItem("username","sourabh")
+
+// navbar colorchange on scroll.......
 window.addEventListener("scroll",()=>{
   if(window.scrollY>100){
     document.querySelector(".nav").classList.add("onscrollnav")
     document.querySelector(".loginbutton").classList.add("loginbuttononscroll")
     document.querySelector(".tutorlogin").classList.add("loginbuttononscroll")
+    
   }
   else{
     document.querySelector(".nav").classList.remove("onscrollnav")
     document.querySelector(".loginbutton").classList.remove("loginbuttononscroll")
     document.querySelector(".tutorlogin").classList.remove("loginbuttononscroll")
+    
   }
 })
+// ..........................................
+
+//user display...................................
+ 
+let username=localStorage.getItem("username")||null
+let userdisplay=document.querySelector(".userdisplay")
+let hamberger=document.querySelector(".hamberger")
+let accountdropdown=document.querySelector(".accountdropdown")
+let count1=0
+if(username){
+  document.querySelector(".loginbutton").classList.add("loginbuttonIflogin")
+  document.querySelector(".tutorlogin").classList.add("tutorloginIflogin")
+  userdisplay.classList.add("userdisplayIflogin")
+  hamberger.classList.add("hambergerIflogin")
+  let firstletter=username[0].toUpperCase()
+  userdisplay.innerHTML=`<h1 class="firstletter">${firstletter}</h1>`
+  document.querySelector(".account").classList.add("accountIflogin")
+  hamberger.addEventListener("click",()=>{
+    count1++
+    console.log(count1%2)
+    if(count1%2!=0){
+    accountdropdown.innerHTML=`
+    <div>My Dashboard</div>
+    <div>Message</div>
+    <div>My profile</div>
+    <div class="logout">Log Out</div>
+    `
+
+    let logout=document.querySelector(".logout")
+    logout.addEventListener("click",()=>{
+      localStorage.removeItem("username")
+      window.location.reload()
+    })
+
+    }
+    else{
+     accountdropdown.innerHTML=null
+    }
+  })
+}
+else{
+  document.querySelector(".loginbutton").classList.remove("loginbuttonIflogin")
+  document.querySelector(".tutorlogin").classList.remove("tutorloginIflogin")
+  userdisplay.classList.remove("userdisplayIflogin")
+  hamberger.classList.remove("hambergerIflogin")
+  document.querySelector(".account").classList.remove("accountIflogin")
+}
+
+// ...............................................
 
 let subjectSelection=document.querySelector(".subject-selection")
 
@@ -27,5 +81,8 @@ subjectSelection.addEventListener("click",()=>{
     subjectDropdown.classList.remove("subject-dropdown-visible")
   }
 })
+
+
+
 
 
